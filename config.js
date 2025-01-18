be502afce6597c87bbb240c59cfd721ad0ee292c
Arilens Official <ismaalozturk@gmail.com>
2025-01-18 15:38:16 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  token: "", // Botunuzun tokeni buraya.
+  logKanalID: "", // Başvuru sonuçlarının gönderileceği log kanal idsi.
+  verilecekRolID: "", // Başvurusu kabul edilenlere verilecek rolün ID.
+  clientId: "" // Botunuzun bot ID'si.
+};
